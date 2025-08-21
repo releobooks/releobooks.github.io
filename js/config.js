e72cbba05bcfc6e06926e133d446aab7f33696b4
Releo <releobusines@gmail.com>
2025-08-21 23:34:07 +0200

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,25 @@
+// ===== CONFIGURACIÓN CENTRALIZADA DE RELEO =====
+// MODIFICAR ESTOS DATOS PARA ACTUALIZAR TODA LA WEB
+
+const config = {
+    // DATOS DE CONTACTO - MODIFICAR AQUÍ
+    contacto: {
+        email: 'releobusines@gmail.com',
+        telefonos: ['623125636', '722449740'],
+        direccion: 'Paterna del Campo 21880 Huelva, España'
+    },
+
+    // REDES SOCIALES - MODIFICAR ENLACES AQUÍ
+    redesSociales: {
+        instagram: 'https://www.instagram.com/releo_esp',
+        facebook: 'https://www.facebook.com/profile.php?id=61579012004628',
+        twitter: 'https://twitter.com/releo_esp',
+        linkedin: 'https://www.linkedin.com/company/releo',
+        iberlibro: 'https://www.iberlibro.com'
+    },
+
+    // MAPA DE GOOGLE MAPS - MODIFICAR URL AQUÍ
+    mapa: {
+        embedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3306.960489495492!2d-6.412527!3d37.422206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd11dbf6b4d1b7f7%3A0x4046f67c16e1420!2sPaterna%20del%20Campo%2C%20Huelva!5e0!3m2!1ses!2ses!4v1724230800000!5m2!1ses!2ses'
+    }
+};
